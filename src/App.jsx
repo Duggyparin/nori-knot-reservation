6f5be6bd44bcfd127ec4c544829090ddb06157f7
Duggyparin <monsanto.bryann@gmail.com>
2026-04-07 20:07:32 +0800

    Fix mobile login with Safari optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { supabase } from './lib/supabase';
-import Login from './pages/LoginSupabase';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Admin from './pages/Admin';
 import Landing from './pages/Landing';
